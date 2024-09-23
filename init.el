82ef870e3331d05d447162ea4af1912a17049b75
Charlie Root <charlie@charlieroot.dev>
2024-09-24 00:08:17 +0200

    init-programming.el: init
    This commit adds init-programming.el, which will set some sane
    programing defualts in the future

--- init.el
+++ init.el
@@ -1,12 +1,33 @@
 ;;; init-el --- The core of my emcas configuration -*- lexical-binding: t -*-
-;;; Commentary
+
+;;; Commentary:
+
+;; Welcome dear emacs-intrested individual! This is my personal emacs configuration,
+;; made to be extensible and as simple as feasuble, as complex as needed
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
 
 ;;; Code:
 
 ;; The entrypoint for all of my configuration files
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
-
+;; set the garbage collection threshold higher, this makes startup a lot faster.
 (setq gc-cons-threshold (* 128 1024 1024))
 
 (setq custom-file (locate-user-emacs-file "custom.el"))
@@ -21,3 +42,4 @@
 (require 'init-vcs)
 (require 'init-themes)
 (require 'init-editor)
+(require 'init-programming)
---
+++ lisp/init-programming.el
@@ -0,0 +1,39 @@
+;;; init-programming.el --- Programming defaults -*- lexical-binding: t -*-
+;;
+;; Copyright @ 2024-2024 Charlie Root
+;;
+;; Author: Charlie Root <charlie@charlieroot.dev>
+;; URL: https://charlieroot.dev
+
+;; This file is not part of GNU Emacs.
+
+;; Commentary:
+
+;;; License
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+
+;; highlight strings like TODO, FIXME, NOTE, HACK, ...
+(use-package hl-todo
+  :diminish
+  :config
+  (global-hl-todo-mode 1))
+
+
+(provide 'init-programming)
+
